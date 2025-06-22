657a1ba0945dababa8e50c7a522d274492696b43
loligoshujinsama <Hazleycryilis@gmail.com>
2025-06-22 16:10:32 +0800

    Create test_basic.py

---
+++ app-server/tests/test_basic.py
@@ -0,0 +1,14 @@
+import pytest
+
+from app import app 
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_status_code(client):
+    """Check if the home page loads correctly (200 OK)"""
+    response = client.get('/home')
+    assert response.status_code == 200
