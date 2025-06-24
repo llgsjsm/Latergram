fbfc50aa4b5d587474c62f9b50615a111137644d
chinliong <chinliong2001@yahoo.com.sg>
2025-06-25 06:56:04 +0800

    define getter setter for follower

--- app-server/models/user.py
+++ app-server/models/user.py
@@ -102,3 +102,31 @@ class Follower(db.Model):
     # Relationships
     follower = db.relationship('User', foreign_keys=[followerUserId], backref='following_relationships')
     followed = db.relationship('User', foreign_keys=[followedUserId], backref='follower_relationships')
+    
+    
+    def get_follower_id(self):
+        return self.followerUserId
+    
+    def set_follower_id(self, follower_id):
+        self.followerUserId = follower_id
+        
+    def get_followed_id(self):
+        return self.followedUserId
+    
+    def set_followed_id(self, followed_id):
+        self.followedUserId = followed_id
+        
+    def get_status(self):
+        return self.status
+
+    def set_status(self, status):
+        self.status = status
+        
+    def get_created_at(self):
+        return self.createdAt
+    
+    def set_created_at(self, created_at):
+        self.createdAt = created_at
+        
+    def get_id(self):
+        return self.id
