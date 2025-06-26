9fa2d3960bbb08c368cc648305fe232f56f837f0
andrealye <73918832+andrealye@users.noreply.github.com>
2025-06-26 13:12:21 +0800

    minor styling fixes and added default user icon for profile following/follow modals

--- app-server/app.py
+++ app-server/app.py
@@ -105,10 +105,15 @@ def log_to_splunk(event_data):
 def ensure_firebase_initialized():
     global bucket
     if not firebase_admin._apps:
-        cred = credentials.Certificate(r'C:\Users\andre\OneDrive\Documents\GitHub\Latergram\latergram-e9a26-firebase-adminsdk-fbsvc-229aa3b1e8.json')
-        firebase_admin.initialize_app(cred, {
-            'storageBucket': BUCKET
-        })
+        file_location = os.environ.get('FILE_LOCATION', '')
+        bucket_name = os.environ.get('BUCKET', '')
+        if file_location and bucket_name:
+            cred = credentials.Certificate(file_location)
+            firebase_admin.initialize_app(cred, {
+                'storageBucket': bucket_name
+            })
+        else:
+            raise RuntimeError('Firebase FILE_LOCATION or BUCKET not set in environment variables')
     bucket = storage.bucket()
 
 @app.route('/home')
