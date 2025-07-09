e9d6a344efd8d06191ac2397b02bcc6d50b508f9
loligoshujinsama <Hazleycryilis@gmail.com>
2025-07-10 05:10:56 +0800

    update to report
    Co-Authored-By: WKXXX <74634179+WKXXX@users.noreply.github.com>
    Co-Authored-By: Sherin <108775903+shafeena-sherin@users.noreply.github.com>

--- app-server/backend/limiter.py
+++ app-server/backend/limiter.py
@@ -8,14 +8,9 @@ request_data = defaultdict(list)
 def check_rate_limit(ip, request_data, max_requests=5, time_window=15):
     """Check if the IP has exceeded the rate limit"""
     current_time = time.time()
-
-    # Clean up the timestamps that are outside of the time window
     request_data[ip] = [timestamp for timestamp in request_data[ip] if current_time - timestamp < time_window]
-
-    # Check if the number of requests exceeds the max allowed
     if len(request_data[ip]) >= max_requests:
         return False  # Rate limit exceeded
-
     return True
 
 def rate_limit_required(func):
@@ -25,8 +20,6 @@ def rate_limit_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         ip = get_real_ip()
-
-        # Check if the request is a POST request
         if request.method == "POST":
             if not check_rate_limit(ip, request_data=request_data):
                 return jsonify({"error": "Rate limit exceeded"}), 429
